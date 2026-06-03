b53c5bedbc23ad88648471ce94d8cadda70c233f
beketovfedor <beketovfedor@gmail.com>
2026-06-03 16:56:02 +0300

    Реализовал класс Ingredient (класс необходим для хранения информации о конкретном ингридиенте, включая его название, единицу измерения и количество)

---
+++ src/Ingredient.py
@@ -0,0 +1,25 @@
+class Ingredient:
+    def __init__(self, name, quantity, unit):
+        self.name = name
+        self.quantity  =quantity
+        self.unit = unit
+
+    @property
+    def quantity(self):
+        return self.quantity
+    
+    @quantity.setter
+    def quantity(self, value):
+        if value <= 0:
+            raise ValueError("Количество должно быть положительным")
+        self.quantity = float(value)
+    
+    def __str__(self):
+        return f"{self.name}: {self.quantity} {self.unit}"
+    def __repr__(self):
+        return f"Ingredient('{self.name}', {self.quantity}, '{self.unit}')"
+    def __eq__(self, other):
+        if isinstance(other, Ingredient):
+            return self.name == other.name and self.unit == other.unit
+        return False
+
